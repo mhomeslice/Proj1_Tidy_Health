f858b17ae2f0d52df480f797f006d4cd5886a5bf
mhomeslice <mikeholmquest@gmail.com>
2022-01-19 15:34:58 -0700

    Initial commit

---
+++ proj1_health/config/Set_up_code.R
@@ -0,0 +1,10 @@
+
+# Set up relative directory navigation
+install.packages("here")
+library(here)
+
+# Set-up Project Template
+install.packages("ProjectTemplate")
+library(ProjectTemplate)
+create.project(project.name = "proj1_health", template = "minimal")
+
